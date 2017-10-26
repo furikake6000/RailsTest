f2aabe8a6ca19fe424c01679ee6b736b70496782
Furikake <ewave63@keio.jp>
2017-10-26 13:00:23 +0900

    ランダム文字列生成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-  	render html: "hello, ruby world!\n"
+  	render html: "hello, ruby world!" + ('a'..'z').to_a.shuffle[0..7].join
   end
 end
